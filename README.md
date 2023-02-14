9fcf7b10a635f61566e511a4557c4a5e4370abb3
shuihua <2416564930@qq.com>
2023-02-14 17:32:50 +0800

    完成分页器Pagination

---
+++ README.md
@@ -0,0 +1,3 @@
+1. 打开项目
+2. pnpm install
+3. pnpn run dev
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import TestPain from './components/TestPagination/TestPain'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <TestPain></TestPain>
+    </div>
+  )
+}
+
+export default App
